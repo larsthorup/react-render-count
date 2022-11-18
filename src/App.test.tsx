22c7890905c6158a2541f470bbb3c55b8f8cc681
Lars Thorup <lars@zealake.com>
2022-11-18 14:23:09 +0100

    testing library

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,12 @@
 import { expect, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-it("should work", () => {
-  expect(2 + 2).toEqual(4);
+import App from "./App";
+
+it("should count when clicked", async () => {
+  render(<App />);
+  expect(screen.getByRole("button").textContent).toEqual("count is 0");
+  await userEvent.click(screen.getByRole("button"));
+  expect(screen.getByRole("button").textContent).toEqual("count is 1");
 });
